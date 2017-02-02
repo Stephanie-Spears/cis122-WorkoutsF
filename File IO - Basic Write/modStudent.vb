ab5727ef2e056d022cb5e334485aaa8494b32103
Stephanie Spears <stephanie.spears1@gmail.com>
2017-02-01 20:04:19 -0800

    load

---
+++ File IO - Basic Write/modStudent.vb
@@ -0,0 +1,54 @@
+﻿Imports System.IO	'MUST HAVE THIS FOR FILE ACCESS
+
+Module modStudent
+
+	Public Sub RunProject()
+
+		'Name: File IO - Basic Write
+		'Purpose: Demonstrates basic file writing
+		'Author: CIS 122
+		'Date: 11/19/14
+
+		'>>> The purpose of this project is to demonstrate basic file OUTPUT. So far, we've been READING from files, but now it's time to WRITE to a file.
+
+		'Constants
+		'>>> Similarly to what we did before, we'll create a constant to store the path of the file we're going to write to.  Keep in mind that the file will be created in the same folder as our .exe file - most likely "bin\Debug".
+		Const c_sOutputFilePath As String = "BasicWrite.txt"
+
+		'Variables
+		Dim sText As String = ""  '>>> We'll use a string a variable to store the text we're going to write to the output file.
+		'>>> When we read from a file, we needed to use a StreamReader variable. But when *writing* to a file, we use a StreamWriter variable. Notice the "file" prefix I'm using to designate that this variable works with files. I've decided to name the file "fileOutput", which makes it nice and clear that we're going to write to it.
+		Dim fileOutput As StreamWriter
+
+
+		'Begin Code
+		SetTitle("File IO - Basic Write")
+
+		'>>> Now we have to create the file we're going to write to. If the file already exists, its contents will be wiped out.
+		'We use "File.CreateText" to create a simple empty text file. It's a function and it expects to be given the path of the output file you want to create. So, we pass it our constant that we created up above.
+		'But that's not all, folks. CreateText returns a special piece of data that we need. It returns a "file handle" - a StreamWriter variable type. We declared this variable up above, and now we're going to make use of it:
+		fileOutput = File.CreateText(c_sOutputFilePath)
+		'>>> From now on in out program, we will use the fileOutput variable to work with the file.
+		'Note: if you want *append* text to a file instead of wiping out the existing contents, use File.AppendText instead of File.CreateText.
+
+
+		'>>> And now it's time to actually write to the file. To do this, we must use our StreamWriter variable, fileOutput. The StreamWriter variable has a special function called "WriteLine" that we can use to write a single line of text to our file. For example:
+		fileOutput.WriteLine("This is the first line of text.")
+		'Note that WriteLine will also write out a newline character after the text, which causes whatever is written next time to begin on the NEXT line, instead of the same line.
+		'Now our text file will contain one line of text.  Let's write some more...
+
+		'>>> Using a For loop, we're going to write out 10 lines of text. Notice the we're using the For loop's "counter" variable (i) as part of our text.  Take a look:
+		For i As Integer = 1 To 10
+			fileOutput.WriteLine("Line " & i)
+		Next
+		'Can you guess what the above code is writing out? Run the program, open the text file with Notepad (or something similar) and find out. Close the file when you're done looking at it.
+
+		'>>> Remember to close the file
+		fileOutput.Close()
+
+
+		'>>> NOTE: To write a blank line to a file, do this:  fileOutput.WriteLine()
+
+	End Sub
+
+End Module
---
+++ File IO - ReadWrite - Scores/modStudent.vb
@@ -0,0 +1,66 @@
+﻿Imports System.IO	'MUST HAVE THIS FOR FILE ACCESS
+
+Module modStudent
+
+	Public Sub RunProject()
+
+		'Name: File IO - ReadWrite -Scores
+		'Purpose: Demonstrates file reading
+		'Author: CIS 122
+		'Date: 11/19/14
+
+		'>>> This program is very similar to the program that read scores from a file and then displayed into to the screen. We're going to do the exact same thing, only this time, we're outputting to a file instead of the screen.  So, we'll be working with TWO files at the same time - our input file (the file we're reading from), and an output file (the file we're writing to).
+
+		'Constants
+		Const c_sInputFilePath As String = "Scores.txt"	'>>> the path to our input file
+		Const c_sOutputFilePath As String = "ScoresReport.txt"	'>>> the path to our output file
+
+		'Variables
+		Dim fileInput As StreamReader	'>>> For the input file, we need a variable of type StreamReader, just as we did before.
+		Dim fileOutput As StreamWriter '>>> For the output file, we need a variable of type StreamWriter, just as we used in the Basic Write workout.
+		Dim sName As String = ""
+		Dim dTotal As Double = 0
+		Dim dScore As Double = 0
+		Dim dAverage As Double = 0
+
+
+		'Begin Code
+		SetTitle("File IO - ReadWrite -Scores")
+
+		'>>> Get our "file handles" so we can work with the files
+		fileInput = File.OpenText(c_sInputFilePath) '>>> open the input file 
+		fileOutput = File.CreateText(c_sOutputFilePath)	'>>> create the output file
+		'>>> Now we'll use the file handles (fileInput and fileOutput) to work with the files
+
+
+		'>>> we use a While loop to read thru the input file. Each iteration of the loop reads everything it needs for ONE student.
+		While fileInput.Peek <> -1	'>>> Remember - this gobbledy-gook just means "As long as there is more to read"
+			sName = fileInput.ReadLine	'>>> Read the student's name and assign it to sName
+			DisplayLine("Name: " & sName)	'>>> For debugging purposes, display the info to the screen
+			fileOutput.WriteLine("Name: " & sName)	'>>> Write the output to the file
+
+			dTotal = 0 '>>> Remember why we have to set dTotal to zero before we go into the For loop?  If we don't, dTotal will keep accumulating from the previous students and give us bad results.
+
+			'>>> We'll use a For loop to read the 5 scores
+			For i As Integer = 1 To 5
+				dScore = CDbl(fileInput.ReadLine) '>>> ReadLine always gives us text, but we have to do math on dScore, so we will convert it to a Double as we read it in.
+				dTotal = dTotal + dScore
+			Next
+
+			dAverage = dTotal / 5 '>>> Calculate the student's average
+			DisplayLine("Student Average: " & FormatNumber(dAverage) & "%") '>>> Display the student's average
+			fileOutput.WriteLine("Student Average: " & FormatNumber(dAverage) & "%") '>>> Write the student's average to the output file.
+
+			DisplayLine() '>>> Display a blank line
+			fileOutput.WriteLine() '>>> Write a blank line
+		End While
+
+		'>>> Close both of the files
+		fileInput.Close()
+		fileOutput.Close()
+
+		'>>> Run the program and take a look at the output file.
+
+	End Sub
+
+End Module
